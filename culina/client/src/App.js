d32e6184de7dbf74ca7fa283c81719000cd62107
shazy89 <erdo.shazy123@gmail.com>
2021-03-29 15:29:29 -0400

    added spinner component

--- culina/client/src/App.js
+++ culina/client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import "./css/style.css";
+
+import Spinner from "./components/layout/Spinner";
+
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <Button className="button">Click ME</Button>
+      <Spinner />
     </div>
   );
 };
--- culina/client/src/index.js
+++ culina/client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
-
+import "./css/style.css";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
