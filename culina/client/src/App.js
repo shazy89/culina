4f167dc93ab1a28e65b39a5cdf52e1b1ae2c2ab1
shazy89 <erdo.shazy123@gmail.com>
2021-03-29 13:58:30 -0400

    integrated sass and css

--- culina/client/src/App.js
+++ culina/client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { Button } from "react-bootstrap";
+import "./css/style.css";
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button className="button">Click ME</Button>
     </div>
   );
 };
