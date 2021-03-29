9f368aa905dcae0c59edecdf4fd50675aa7b1f60
shazy89 <erdo.shazy123@gmail.com>
2021-03-29 16:59:33 -0400

    handle form

--- culina/client/src/components/layout/LoginDev.js
+++ culina/client/src/components/layout/LoginDev.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Form, Button } from "react-bootstrap";
 
-const LoginDev = () => {
+const LoginDev = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    alert(email);
+    // login(email, password);
+  };
+
+  //if (isAuthenticated) {
+  //  return <Redirect to="/dashboard" />;
+  //}
   return (
     <section className="login">
-      <div className="culina__login--card">
+      <div className="culina__auth--card">
         <div>
-          <h1>Sign Into Culina Dev</h1>
+          <h1 className="culina__auth--header">Sign Into Culina Dev</h1>
         </div>
-        <div></div>
+        <div>
+          <form className="culina__auth--form" onSubmit={onSubmit}>
+            <div className="culina__auth--form-group">
+              <input
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="culina__auth--form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={onChange}
+                minLength="6"
+              />
+            </div>
+            <Button type="submit" variant="primary" className="btn btn-primary">
+              Log In
+            </Button>
+          </form>
+        </div>
       </div>
     </section>
   );
@@ -18,7 +64,7 @@ const LoginDev = () => {
 
 LoginDev.propTypes = {
   login: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool,
+  //  isAuthenticated: PropTypes.bool,
 };
 
 export default LoginDev;
