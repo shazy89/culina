e73fb829b1a565136c14ef13350d55cc018d95aa
shazy89 <erdo.shazy123@gmail.com>
2021-04-01 12:14:38 -0400

    sidebar design

--- culina/client/src/components/layout/SideNav.js
+++ culina/client/src/components/layout/SideNav.js
@@ -2,12 +2,13 @@ import React from "react";
 import { FilePlus } from "react-feather";
 
 const SideNav = () => {
+  // side-nav__link--active
   return (
     <div className="sidebar">
       <ul className="side-nav">
         <li className="side-nav__item">
-          <a className="side-nav__link" href="">
-            <FilePlus /> Click
+          <a className="side-nav__link" href="#">
+            <FilePlus className="side-nav__icon" /> Click
           </a>
         </li>
       </ul>
