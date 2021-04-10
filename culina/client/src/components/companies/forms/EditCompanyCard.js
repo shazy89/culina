e23077675e04f23771c18b8d33b738f4898bb899
shazy89 <erdo.shazy123@gmail.com>
2021-04-10 12:02:40 -0400

    styling ot the form

--- culina/client/src/components/companies/forms/EditCompanyCard.js
+++ culina/client/src/components/companies/forms/EditCompanyCard.js
@@ -27,7 +27,7 @@ const EditCompanyCard = ({
   company: { name, email, adress, phone, timeZone }
 }) => {
   const [formData, setFormData] = useState(initialState);
-
+  const onChange = () => {};
   return (
     <div className="company__info">
       {" "}
@@ -40,37 +40,57 @@ const EditCompanyCard = ({
             <div className="u-margin-top">
               <label className="company__info--header">
                 {" "}
-                <Field className="text-center" name="textA" value={name} />
+                <Field
+                  onChange={onChange}
+                  className="text-center edit__input--field"
+                  name="textA"
+                  placeholder="Name"
+                  value={name}
+                />
               </label>
             </div>
             <div className="u-margin-top-big font-size-normal">
               <label>
-                textB
                 <Field
-                  className="text-center"
+                  className="text-center edit__input--field"
                   type="email"
                   name="email"
                   value={email}
                   placeholder="Email"
+                  onChange={onChange}
                 />
               </label>
             </div>
             <div className="u-margin-top font-size-normal">
+              <label className="font-size-normal">Contact Number:</label>
+              <Field
+                onChange={onChange}
+                className="edit__input--field"
+                value={phone}
+                placeholder="Phone"
+                name="phone"
+              />
+              <br />
+              <br />
               <label className="font-size-normal">
-                textB
-                <Field className="text-center" name="textB" />
-              </label>
-              <br />
-              <br />
-              <label className="text-center font-size-normal">
-                textB
-                <Field name="textB" />
+                <Field
+                  onChange={onChange}
+                  value={timeZone}
+                  className="text-center edit__input--field"
+                  placeholder="TimeZone"
+                  name="TimeZone"
+                />
               </label>
             </div>
             <div>
               <label className="font-size-normal">
-                textB
-                <Field className="text-center " name="textB" />
+                <Field
+                  onChange={onChange}
+                  className="text-center edit__input--field"
+                  value={adress}
+                  placeholder="Adress"
+                  name="Adress"
+                />
               </label>
             </div>
 
