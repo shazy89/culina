2e002e2fa1fc962b3456070684e3d45cc31b1f01
shazy89 <erdo.shazy123@gmail.com>
2021-04-10 18:01:10 -0400

    edit company

--- culina/client/src/components/companies/forms/EditCompanyCard.js
+++ culina/client/src/components/companies/forms/EditCompanyCard.js
@@ -8,6 +8,7 @@ import {
   useFormikContext,
   MyField
 } from "formik";
+import { useForm } from "react-hook-form";
 import { Button } from "react-bootstrap";
 const initialState = {
   name: "",
@@ -25,10 +26,12 @@ const initialState = {
 
 const EditCompanyCard = ({ company, className }) => {
   const [formData, setFormData] = useState(initialState);
+
+  const { name, email, adress, phone, timeZone } = formData;
+
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const { name, email, adress, phone, timeZone } = formData;
   useEffect(() => {
     if (company) {
       const companyData = { ...initialState };
@@ -41,82 +44,82 @@ const EditCompanyCard = ({ company, className }) => {
   }, [company]);
   const onSubmit = (e) => {
     e.preventDefault();
-    debugger;
   };
 
   return (
     <div className="company__info">
-      {" "}
-      <Formik
-        initialValues={formData}
-        onSubmit={async (values) => console.log(values)}
-      >
-        <div>
-          <Form className={"edit--form--section " + className}>
-            <div className="u-margin-top">
-              <label>
-                {" "}
-                <Field
-                  onChange={onChange}
-                  className="text-center edit__input--field company__info--header"
-                  name="name"
-                  placeholder="Name"
-                  value={name}
-                />
-              </label>
-            </div>
-            <div className="u-margin-top-big font-size-normal">
-              <label>
-                <Field
-                  className="text-center edit__input--field"
-                  type="email"
-                  name="email"
-                  value={email}
-                  placeholder="Email"
-                  onChange={onChange}
-                />
-              </label>
-            </div>
-            <div className="u-margin-top font-size-normal">
-              <label className="font-size-normal">
-                <Field
-                  onChange={onChange}
-                  className="edit__input--field text-center"
-                  value={phone}
-                  placeholder="Phone"
-                  name="phone"
-                />
-              </label>
-              <br />
-              <br />
-              <label className="font-size-normal">
-                <Field
-                  onChange={onChange}
-                  value={timeZone}
-                  className="text-center edit__input--field"
-                  placeholder="TimeZone"
-                  name="timeZone"
-                />
-              </label>
-            </div>
-            <div>
-              <label className="font-size-normal">
-                <Field
-                  onChange={onChange}
-                  className="text-center edit__input--field"
-                  value={adress}
-                  placeholder="Adress"
-                  name="adress"
-                />
-              </label>
-            </div>
+      <div>
+        <form
+          onSubmit={onSubmit}
+          className={"edit--form--section " + className}
+        >
+          <div className="u-margin-top">
+            <label>
+              {" "}
+              <input
+                onChange={onChange}
+                className="text-center edit__input--field company__info--header"
+                name="name"
+                placeholder="Name"
+                value={name}
+              />
+            </label>
+          </div>
+          <div className="u-margin-top-big font-size-normal">
+            <label>
+              <input
+                className="text-center edit__input--field"
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={onChange}
+              />
+            </label>
+          </div>
+          <div className="u-margin-top font-size-normal">
+            <label className="font-size-normal">
+              <input
+                onChange={onChange}
+                className="edit__input--field text-center"
+                value={phone}
+                placeholder="Phone"
+                name="phone"
+              />
+            </label>
+            <br />
+            <br />
+            <label className="font-size-normal">
+              <input
+                onChange={onChange}
+                value={timeZone}
+                className="text-center edit__input--field"
+                placeholder="TimeZone"
+                name="timeZone"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="font-size-normal">
+              <input
+                onChange={onChange}
+                className="text-center edit__input--field"
+                value={adress}
+                placeholder="Adress"
+                name="adress"
+              />
+            </label>
+          </div>
 
-            <Button className="u-margin-top font-size-normal" variant="primary">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </Formik>
+          <Button
+            className="u-margin-top font-size-normal"
+            type="submit"
+            variant="primary"
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
