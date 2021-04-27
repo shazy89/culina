49ede6c5fab5f99ddb28ed7c8fdf7fe7fbdb9dae
shazy89 <erdo.shazy123@gmail.com>
2021-04-27 16:22:44 +0200

    save the new project and prepare the company fields

--- culina/controllers/Project.js
+++ culina/controllers/Project.js
@@ -1,4 +1,5 @@
 const Project = require("../models/Project");
+const Company = require("../models/newCompany");
 const normalizeData = require("../services/normalizeData");
 
 // New Project  culina/companyId/project
@@ -8,13 +9,38 @@ exports.newOrUpdate = async function (
 ) {
   const { companyId, ...rest } = body;
 
-  const userFields = {
+  const projectFields = {
     companyId: company,
     ...rest
   };
 
   try {
     //Note to do later if its not a company user cannot create a new project for this company!!
+    if (user.admin || (position === "Manager" && company === user.company)) {
+      const project = await Project.findOne({ _id });
+
+      if (project) {
+        const existingUser = await Project.findOneAndUpdate(
+          { _id: _id }, // filter
+          { $set: projectFields }, // update
+          { new: true }
+        );
+        //return the updated user
+        return res.json(existingUser);
+      }
+      const newProject = await new CompanyUser(userFields);
+      const company = await Company.findOne({ _id: id });
+
+      await newProject.save();
+
+      const companyUserFields = await {
+        userId: newUser._id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        avatar: newUser.avatar,
+        position: newUser.position
+      };
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
