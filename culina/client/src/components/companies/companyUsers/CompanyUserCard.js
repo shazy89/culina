c2e6d1ce878ed053c45d8fc8faa556f039ec8fb8
shazy89 <erdo.shazy123@gmail.com>
2021-05-03 15:14:31 +0200

    edit project and projectinput boilerplate

--- culina/client/src/components/companies/companyUsers/CompanyUserCard.js
+++ culina/client/src/components/companies/companyUsers/CompanyUserCard.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+
+import {
+  Image as CloudinaryImage,
+  Video,
+  Transformation,
+  CloudinaryContext
+} from "cloudinary-react";
 
 const CompanyUserCard = ({ info: { firstName, lastName, avatar } }) => {
   return (
     <div className="slider--box">
-      <Image
+      <CloudinaryImage
         className="slider--circle__image"
-        src={avatar}
-        roundedCircle
-        thumbnail
-      />
+        cloudName="dytheecsk"
+        publicId="portfolio/me_ocryct.jpg"
+      >
+        <Transformation width="130" crop="scale" />
+      </CloudinaryImage>
 
       <h2>{firstName + " " + lastName}</h2>
     </div>
@@ -21,3 +28,9 @@ export default CompanyUserCard;
 //<div className="slider--circle">
 //<img className="slider--circle__image" src={avatar} alt="user image" />
 //</div>
+//    <RoundedCircle
+//    className="slider--circle__image"
+//    src="https://res.cloudinary.com/dytheecsk/image/upload/v1616517786/Friends/eizimamzohzcdpb3lw2y.jpg"
+//    roundedCircle
+//    thumbnail
+//    />
