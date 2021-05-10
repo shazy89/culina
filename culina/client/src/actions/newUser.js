479e70fbe2eb05c2f44f83a5698ed7e74ea0b088
shazy89 <erdo.shazy123@gmail.com>
2021-05-10 05:29:26 +0200

    boilerplate

---
+++ culina/client/src/actions/newUser.js
@@ -0,0 +1,12 @@
+export const newUser = (userInfo) => async (dispatch) => {
+  try {
+  } catch (error) {}
+};
+export const a = (userInfo) => async (dispatch) => {
+  try {
+  } catch (error) {}
+};
+export const b = (userInfo) => async (dispatch) => {
+  try {
+  } catch (error) {}
+};
