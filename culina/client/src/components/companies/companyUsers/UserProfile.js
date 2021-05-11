5e56fa0c5ba979d48383104b91788f40cb48594a
shazy89 <erdo.shazy123@gmail.com>
2021-05-11 08:48:17 +0200

    fetch user data

--- culina/client/src/components/companies/companyUsers/UserProfile.js
+++ culina/client/src/components/companies/companyUsers/UserProfile.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "utils/api";
 import PropTypes from "prop-types";
 
-const UserProfile = ({ match }) => {
+const UserProfile = ({
+  match: {
+    params: { id, userId }
+  }
+}) => {
+  const [profileInfo, setprofileInfo] = useState("");
+  console.log(profileInfo);
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const res = await api.get(`/companies/${id}/user/${userId}`);
+      setprofileInfo(res.data);
+    };
+    fetchUserInfo();
+  }, []);
   return <div>HEY USER</div>;
 };
 
